c9b54e1d15dedf46c3647e21a3f2640598dceb75
Doniyor Ziyayev <donyziyayev1989@users.noreply.github.com>
2024-11-12 19:30:34 +0500

    added toggle, edit, delete function to task

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,29 +2,68 @@ const { Component, mount, xml, useState } = owl;
 
 class Task extends Component {
   static template = xml`
-         <li t-attf-style="background-color:#{props.task.color}" class="list-group-item d-flex justify-content-between align-items-center form-check">
-            <div class="form-check me-auto mb-0">
+    <li t-attf-style="background-color:#{props.task.color}" class="list-group-item d-flex justify-content-between align-items-center form-check">
+        <div t-if="state.isEditing" class="input-group d-flex flex-fill align-items-center " style="width:auto">
+            <input
+                type="text"
+                id="todo"
+                class="form-control flex-fill "
+                placeholder="Add Todo"
+                t-model="state.name"
+            />
+            <input
+                type="color"
+                class="form-control-color ms-2 rounded"
+                style="background:white"
+                t-att-value="props.task.color"
+                t-model="state.color"
+            />
+        </div>
+        <div t-if="!state.isEditing" class="form-check me-auto mb-0">
             <input
                 class="form-check-input"
                 type="checkbox"
-                value=""
-                t-att-id="props.task.id"
+                t-att-id="state.id"
+                t-att-value="state.isCompleted"
+                t-on-click="toggleTask"
             />
-            <label class="form-check-label" t-att-for="props.task.id">
-                <t t-esc="props.task.name"/>
+            <label class="form-check-label" t-att-for="state.id" t-attf-class="#{state.isCompleted ? 'text-decoration-line-through' : ''}">
+                <t t-esc="state.name"/>
             </label>
-            </div>
-            <div>
-            <button class="btn btn-warning mx-2">
-                <i class="bi bi-pencil"></i>
-            </button>
-            <button class="btn btn-danger">
-                <i class="bi bi-trash"></i>
-            </button>
-            </div>
-        </li>
-    `;
-  static props = ["task"];
+        </div>
+        <div>
+        <button class="btn btn-warning mx-2" t-on-click="toggleEdit">
+            <t t-if="state.isEditing"><i class="bi bi-floppy"></i></t>
+            <t t-else=""><i class="bi bi-pencil"></i></t>
+        </button>
+        <button class="btn btn-danger" t-on-click="deleteTask">
+            <i class="bi bi-trash"></i>
+        </button>
+        </div>
+    </li>
+`;
+  static props = ["task", "onDelete", "onEdit"];
+
+  setup() {
+    this.state = useState({
+      id: this.props.task.id,
+      name: this.props.task.name,
+      color: this.props.task.color,
+      isCompleted: this.props.task.isCompleted,
+      isEditing: false,
+    });
+  }
+  toggleTask() {
+    this.state.isCompleted = !this.state.isCompleted;
+  }
+  deleteTask() {
+    this.props.onDelete(this.props.task);
+  }
+  toggleEdit() {
+    this.state.isEditing = !this.state.isEditing;
+    this.props.onEdit(this.state);
+    console.log(this.state);
+  }
 }
 
 class Root extends Component {
@@ -48,7 +87,7 @@ class Root extends Component {
                 />
                 <input
                   type="color"
-                  class="form-control form-control-color mx-2 border-0 input-sm"
+                  class="form-control-color mx-2 border-0 input-sm"
                   name=""
                   id=""
                   t-att-value="state.color"
@@ -62,7 +101,7 @@ class Root extends Component {
             
               <ul class="list-group" id="todo-list">
                 <t t-foreach="tasks" t-as="task" t-key="task.id">
-                   <Task task="task"/>
+                   <Task task="task" onDelete.bind="deleteTask" onEdit.bind="editTask"/>
                 </t>
               </ul>
             </div>
@@ -98,6 +137,15 @@ class Root extends Component {
 
     // after submit clean inputs
     this.state = { ...this.state, name: "", color: "#ffff00" };
+  }
+  deleteTask(task) {
+    const index = this.tasks.findIndex((t) => t.id == task.id);
+    this.tasks.splice(index, 1);
+  }
+  editTask(task) {
+    const index = this.tasks.findIndex((t) => t.id == task.id);
+    this.tasks.splice(index, 1, task);
+    console.log(this.tasks);
   }
 }
 
